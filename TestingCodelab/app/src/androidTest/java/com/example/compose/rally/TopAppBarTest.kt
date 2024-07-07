dfd9cb75e4f03ace5cbf2245741b8a1b6b983e66
Wonhee Jang <Qtsky89@gmail.com>
2024-07-07 20:37:55 +0900

    https://developer.android.com/codelabs/jetpack-compose-testing#3 done

--- TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,6 +1,9 @@
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.RallyScreen
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
@@ -25,5 +28,23 @@ class TopAppBarTest {
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
     }
 }

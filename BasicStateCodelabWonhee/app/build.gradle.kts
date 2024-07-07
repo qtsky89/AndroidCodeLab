421def186968eaeec67a45039d011b6d35a84d31
Wonhee Jang <Qtsky89@gmail.com>
2024-07-07 20:02:38 +0900

    Added missing dep

--- BasicStateCodelabWonhee/app/build.gradle.kts
+++ BasicStateCodelabWonhee/app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)

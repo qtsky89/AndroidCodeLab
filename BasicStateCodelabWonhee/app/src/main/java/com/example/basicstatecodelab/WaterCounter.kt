de41b39b63dd00593e79b56b68096142f36a6494
Wonhee Jang <Qtsky89@gmail.com>
2024-07-07 17:25:11 +0900

    https://developer.android.com/codelabs/jetpack-compose-state#9 done

--- BasicStateCodelabWonhee/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelabWonhee/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,18 +1,16 @@
 package com.example.basicstatecodelab
 
-import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
---
+++ BasicStateCodelabWonhee/app/src/main/java/com/example/basicstatecodelab/WellnessTaskLists.kt
@@ -0,0 +1,23 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+
+fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+@Composable
+fun WellnessTasksList(
+    modifier: Modifier = Modifier,
+    list: List<WellnessTask> = rememberSaveable { getWellnessTasks() }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) { task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+    }
+}

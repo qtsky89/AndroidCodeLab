29789c40864dbca761b893a58e4fd6e223ed5b10
Wonhee Jang <Qtsky89@gmail.com>
2024-07-07 14:35:49 +0900

    https://developer.android.com/codelabs/jetpack-compose-state#3 done

--- BasicStateCodelabWonhee/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelabWonhee/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,5 +1,7 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,9 +10,13 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    var count = 0
+    Column (modifier = modifier.padding(16.dp)){
+        Text(
+            text = "You've had $count glasses."
+        )
+        Button(onClick = { count++}, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }

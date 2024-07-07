1b2b80f2afdb8198fd94d641760444cfc72b9448
Wonhee Jang <Qtsky89@gmail.com>
2024-07-07 16:37:46 +0900

    https://developer.android.com/codelabs/jetpack-compose-state#7 done

--- BasicStateCodelabWonhee/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelabWonhee/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
@@ -16,7 +17,7 @@ import androidx.compose.ui.Modifier
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Text("You've had $count glasses.")
         }

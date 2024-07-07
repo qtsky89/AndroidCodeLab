45062a745e6d9e13ed2aa7e3a4848f200bf816aa
Wonhee Jang <Qtsky89@gmail.com>
2024-07-07 14:48:00 +0900

    used by keyword

--- BasicStateCodelabWonhee/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelabWonhee/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -6,20 +6,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
+    var count by remember { mutableStateOf(0)}
 
-    val count: MutableState<Int> = remember { mutableStateOf(0) }
     Column (modifier = modifier.padding(16.dp)){
         Text(
-            text = "You've had ${count.value} glasses."
+            text = "You've had ${count} glasses."
         )
-        Button(onClick = { count.value++}, Modifier.padding(top = 8.dp)) {
+        Button(onClick = { count++}, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
